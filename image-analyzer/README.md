63a7dfab4ac63d2174492e5d0b6acfe1ce6dc505
mbusila-unity3d <marian.busila@unity3d.com>
2024-02-20 13:28:25 -0500

    convert image analyzer to a streamlit app

--- image-analyzer/README.md
+++ image-analyzer/README.md
@@ -3,7 +3,22 @@
 Image analyzer with [Imp-V1-3B](https://huggingface.co/MILVLG/imp-v1-3b) MSLM (multimodal small language model), crafted by integrating a compact yet formidable small language model, [Phi-2](https://huggingface.co/microsoft/phi-2), and an advanced visual encoder, [SigLIP](https://huggingface.co/google/siglip-base-patch16-384).
 It provides similar performance to the much larger LLaVA-7B model.
 
+<p float="left">
+  <img src="img/office_q2.png" width="40%" />
+  <img src="img/office_q1.png" width="40%" /> 
+</p>
+
+<p float="left">
+  <img src="img/u-stock.png" width="40%" />
+  <img src="img/game-of-thrones.png" width="40%" />
+</p>
+
+
 Source: [Imp-V1-3B: How a Tiny Model is Beating Giants in Multimodal LLM Space](https://www.youtube.com/watch?v=XPCIH1MkcCU)
+
+Using:
+- [streamlit](https://github.com/streamlit/streamlit) - build webapps in minutes
+- [Imp-V1-3B](https://huggingface.co/MILVLG/imp-v1-3b) - multimodal small language model
 
 Setup
 
@@ -22,5 +37,5 @@ nvcc --version
 python -m venv venv
 venv\Scripts\activate
 pip install -r requirements.txt
-python image-analyzer.py
+streamlit run image-analyzer.py
 ```
--- image-analyzer/image-analyzer.py
+++ image-analyzer/image-analyzer.py
@@ -1,32 +1,55 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer
 from PIL import Image
+import streamlit as st
 
-torch.set_default_device('cuda')
 
-#Create model and tokenizer
-model = AutoModelForCausalLM.from_pretrained("MILVLG/imp-v1-3b", 
-                                             torch_dtype = torch.float16, 
-                                             device_map = "cuda:0", 
-                                             trust_remote_code = True)
+def analyze_image(image, question):
+    torch.device('cuda')
 
-tokenizer = AutoTokenizer.from_pretrained("MILVLG/imp-v1-3b", trust_remote_code = True)
+    #Create model and tokenizer
+    model = AutoModelForCausalLM.from_pretrained("MILVLG/imp-v1-3b", 
+                                                torch_dtype = torch.float16, 
+                                                device_map = "cuda:0", 
+                                                trust_remote_code = True)
 
-#Set inputs
-text = """A chat between a curious user and an artificial intelligence assistant. 
-The assistant gives helpful, detailed, and polite answers to the user's questions. 
-USER: <image>\nAnalyze the image. 
-ASSISTANT:"""
-image = Image.open("data/office.jpg")
+    tokenizer = AutoTokenizer.from_pretrained("MILVLG/imp-v1-3b", trust_remote_code = True)
 
-input_ids = tokenizer(text, return_tensors='pt').input_ids
-image_tensor = model.image_preprocess(image)
+    #Set inputs
+    text = """A chat between a curious user and an artificial intelligence assistant. 
+    The assistant gives helpful, detailed, and polite answers to the user's questions. 
+    USER: <image>\n.{question}
+    ASSISTANT:""".format(question=question)    
 
-#Generate the answer
-output_ids = model.generate(
-    input_ids,
-    max_new_tokens=300,
-    images=image_tensor,
-    use_cache=True)[0]
+    input_ids = tokenizer(text, return_tensors='pt').input_ids
+    input_ids = input_ids.to('cuda')
+    image_tensor = model.image_preprocess(image)
 
-print(tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip())
+    #Generate the answer
+    output_ids = model.generate(
+        input_ids,
+        max_new_tokens=300,
+        images=image_tensor,
+        use_cache=True)[0]
+
+    return tokenizer.decode(output_ids[input_ids.shape[1]:], skip_special_tokens=True).strip()
+
+
+st.title("Image Question Answering App")
+
+# Upload image
+uploaded_image = st.file_uploader("Upload Image", type=["jpg", "jpeg", "png"])
+
+# Display uploaded image
+if uploaded_image is not None:
+    st.image(uploaded_image, caption="Uploaded Image", use_column_width=True)
+
+# Collect question
+question = st.text_input("Enter your question:")
+
+# Ask button
+if st.button("Ask"):    
+    image = Image.open(uploaded_image)
+    answer = analyze_image(image, question)
+    st.text_area(label="Answer", value = answer, height=100)
+
